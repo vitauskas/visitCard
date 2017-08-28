91140701d14e1d8849966a820bc78440d06d48c5
vitauskas <marselvipp@gmail.com>
2017-08-28 15:14:54 +0300

    Work mail
    Working mail and a modified form

--- js/common.js
+++ js/common.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-	$("#form").submit(function() {
+	$("#top_form").submit(function() {
 		$.ajax({
 			type: "POST",
 			url: "js/mail.php",
 			data: $(this).serialize()
 		}).done(function() {
 			$(this).find("input").val("");
-			alert("Ваше письмо отправлено.");
+			alert("Спасибо. Ваше письмо отправлено!");
 			$("#form").trigger("reset");
 		});
 		return false;
--- js/mail.php
+++ js/mail.php
@@ -1,12 +1,12 @@
 <?php
 
 $recepient = 'alycasar@gmail.com';
-$sitename = 'Название сайта';
+$sitename = 'резюме';
 
 $name = trim($_POST["name"]);
-$phone = trim($_POST["email"]);
+$email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Имя: $name \nПочта: $email \nТекст: $text";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
